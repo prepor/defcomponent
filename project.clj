fd546cad86c4905a6a643434c24be1959939e719
Andrew Rudenko <ceo@prepor.ru>
2015-09-22 15:26:22 +0300

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defcomponent "0.1.3-SNAPSHOT"
+(defproject defcomponent "0.1.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
