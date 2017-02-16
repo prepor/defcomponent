2e473990c401a5ac89b48d74d1ddfebf34dd694d
Roma Sokolov <sokolov.r.v@gmail.com>
2017-02-16 19:01:29 +0300

    [build] отключаем подписи
    tcrawley 02:02:54:
    clojars still respects signatures, and lein check will tell you if you have unsigned deps,
    but signing releases is security theatre w/o better networks of trust
    
    https://clojurians-log.clojureverse.org/clojars/2016-11-07.html

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.2.3"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :sign-releases false}]])
